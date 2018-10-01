0a8194509317769036085588d08a41cb080833fc
Larry Page <larry@Larrys-MBP.PageLibrary>
2018-10-01 19:59:00 -0400

    Event popup

--- templates/calendarJsDefaults.js
+++ templates/calendarJsDefaults.js
@@ -1,18 +1,22 @@
 module.exports = {
     header: {
-        left: '',
-        center: 'prev title next',
-        right: ''
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,basicWeek,basicDay'
     },
     eventClick: function(calEvent, jsEvent, view) {
-        alert(`${calEvent.title}: ${calEvent.description}`);
-    /*      $('<div></div>>').dialog({
-        title: calEvent.title,
-        modal: true,
-        open: () => {
-            $(this).html(calEvent.description);
-        }
-        });
-    */      return false;
+        $('#eventTitle').html('');
+        $('#eventStart').html('');
+        $('#eventEnd').html('');
+        $("#eventDescription").html('');
+        $("#eventLocation").html('');
+        
+        $('#eventTitle').html(`<B>${calEvent.title}</B>`);
+        if(calEvent.start) $('#eventStart').html(`Start: ${moment(calEvent.start).format('MMM Do h:mm A')}`)
+        if (calEvent.end) $('#eventEnd').html(`End: ${moment(calEvent.end).format('MMM Do h:mm A')}`);
+        if (calEvent.description) $("#eventDescription").html(`Description: ${calEvent.description}`);
+        if (calEvent.location) $("#eventLocation").html(`Location: ${calEvent.location}`);
+          $("#eventContent").dialog({ modal: true });
+          return false;
     },
 };
